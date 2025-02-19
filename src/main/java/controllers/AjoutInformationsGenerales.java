4d246cbd29458e246034b28a71909bff0e53edc8
rouaslama1 <roua.slama@esprit.tn>
2025-02-19 17:30:04 +0100

    SceneBuilder updated work

---
+++ src/main/java/controllers/AjoutInformationsGenerales.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AjoutInformationsGenerales {
+    @FXML
+    private TextField tailleField;
+
+    @FXML
+    private TextField poidsField;
+
+    @FXML
+    private ComboBox<String> maladiesCombo;
+
+    @FXML
+    private ComboBox<String> cardioCombo;
+
+    @FXML
+    private ComboBox<String> asthmeCombo;
+
+    @FXML
+    private ComboBox<String> dentaireCombo;
+
+    @FXML
+    private ComboBox<String> chirurgieCombo;
+
+    @FXML
+    private ComboBox<String> allergiesCombo;
+
+    @FXML
+    private TextField professionField;
+
+    @FXML
+    private ComboBox<String> stressCombo;
+
+    @FXML
+    private ComboBox<String> sommeilCombo;
+
+    @FXML
+    private ComboBox<String> activiteCombo;
+
+    @FXML
+    private ComboBox<String> situationCombo;
+}
+
